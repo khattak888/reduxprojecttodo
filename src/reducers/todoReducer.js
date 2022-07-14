eae9c94d9c08b9cc74104780f296204922c4f23a
khattak888 <bilalktk868@gmail.com>
2022-07-14 00:08:47 -0700

    31

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,35 @@
+const initialData = {
+  list: [],
+};
+
+const todoReducer = (state = initialData, action) => {
+  switch (action.type) {
+    case "ADDTODO":
+      const { id, data } = action.payload;
+      return {
+        ...state,
+        list: [
+          ...state.list,
+          {
+            id: id,
+            data: data,
+          },
+        ],
+      };
+
+    case "DELETETODO":
+      const newList = state.list.filter((elem) => elem.id != action.id);
+      return {
+        ...state,
+        list: newList,
+      };
+    case "REMOVETODO":
+      return {
+        ...state,
+        list: [],
+      };
+    default:
+      return state;
+  }
+};
+export default todoReducer;
